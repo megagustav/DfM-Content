58f70ee078092e42dc9296c2795fd1910f3df8d6
Gustav Moorhouse <g.moorhouse@me.com>
2020-08-30 18:42:35 +0200

    Update terms-generator.php

--- survey2/terms-generator.php
+++ survey2/terms-generator.php
@@ -5,7 +5,7 @@
     $.each(terms, function(i, item){
         let match = item.description.match(/_(.+)_/);
         let result = item.description.replace(match[0], '<span class="word">' + match[1] + '</span>');
-    $('ol.generated').append('<li class="definition" id="' + item.wordId + '">' + result + '<sup class="source"><a href="#' + item.source + '">' + item.source + '</a></sup>' + '</li>' );
+    $('ol.sorting.generated').append('<li class="definition" id="' + item.wordId + '">' + result + '<sup class="source"><a href="#' + item.source + '">' + item.source + '</a></sup>' + '</li>' );
     });
     
 </script>
