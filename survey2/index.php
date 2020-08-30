47af4b2f2b2fb3cb9a36fbb3b96b1ac9f2551422
Gustav Moorhouse <g.moorhouse@me.com>
2020-08-30 18:59:27 +0200

    Update index.php

--- survey2/index.php
+++ survey2/index.php
@@ -13,11 +13,10 @@
 
       <div class="row">
             <div class="col-12">
-            <?php include "terms-generator.php";?>
               <ol class="sorting generated">
-                
-                <li class="definition" id="9999">Test test <span class="word">ring</span> has an Akoya cultured pearl with a band of bezel-set round diamonds making it perfect for her to wear to work or the night out.<sup class="source"><a href="#123">123</a></sup></li>
-                <li class="definition toggledef" id="1234">Test test <span class="word highlight">ring</span> has an Akoya cultured pearl with a band of bezel-set round diamonds making it perfect for her to wear to work or the night out.<sup class="source"><a href="#123">123</a></sup></li>
+                <?php include "terms-generator.php";?>
+                <li class="definition" id="9999">Test test <span class="word">hard coded test 1</span> has an Akoya cultured pearl with a band of bezel-set round diamonds making it perfect for her to wear to work or the night out.<sup class="source"><a href="#123">123</a></sup></li>
+                <li class="definition toggledef" id="1234">Test test <span class="word highlight">hardcore 2</span> has an Akoya cultured pearl with a band of bezel-set round diamonds making it perfect for her to wear to work or the night out.<sup class="source"><a href="#123">123</a></sup></li>
               </ol>
             </div>
             <div class="col-4">
