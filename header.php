f7971584271e0ddebbed156bd11fda6edee73b89
Gustav Moorhouse <g.moorhouse@me.com>
2020-08-30 15:45:32 +0200

    moved jquery to header

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="/style/style.css">
   <link rel="stylesheet" type="text/css" href="/survey2/css/styles.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
   <script src=""></script>
   
--- survey2/scripts.php
+++ survey2/scripts.php
@@ -1,6 +1,5 @@
 <footer>
-	
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
     <script src="js/application.js"></script>
     <script src="js/jquery-sortable-min.js"></script>
   
