a4471c54ec71520eb8e532ccc4027e09c285bf2f
Gustav Moorhouse <g.moorhouse@me.com>
2020-09-28 19:45:22 +0200

    removed bad terms

--- survey2/terms.php
+++ survey2/terms.php
@@ -1,21 +1,21 @@
 let terms = [
         {
-                "description": "Meaning as a quality of _interaction_ remains elusive.",
+                "description": "_Interaction_ action that occurs as two or more objects have an effect upon one another. The idea of a two-way effect is essential in the concept of interaction, as opposed to a one-way causal effect.",
                 "wordId": "1",
                 "source": "1"
         },
         {
-                "description": "_Purpose_ is [...] also referred to as the motivational component.",
+                "description": "_Purpose_ is the reason for which something is done or created or for which something exists.",
                 "wordId": "2",
                 "source": "1"
         },
         {
-                "description": "We use _coherence_ to denote the extent to which one's experiences make sense [...]. It is the moment when we exclaim 'I see what you did there' or state 'that made sense to me'.",
+                "description": "_Coherence_ is the quality of being logical and consistent.",
                 "wordId": "3",
                 "source": "1"
         },
         {
-                "description": "The lack of _connectedness_ makes it difficult for affected people to make sense of the situation and understand the other person's significance to them.",
+                "description": "The lack of _Connectedness_ makes it difficult for affected people to make sense of the situation and understand the other person's significance to them.",
                 "wordId": "4",
                 "source": "1"
         },
@@ -25,37 +25,37 @@ let terms = [
                 "source": "1"
         },
         {
-                "description": "We refer to _significance_ as [...] activities and experiences that [may] bring little to no pleasure — [...] yet we deem them deeply important.",
+                "description": "We refer to _Significance_ as [...] activities and experiences that [may] bring little to no pleasure — [...] yet we deem them deeply important.",
                 "wordId": "6",
                 "source": "1"
         },
         {
-                "description": "Purpose as a future-oriented _goal_ can lend significance to the present moment.",
+                "description": "Purpose as a future-oriented _Goal_ can lend significance to the present moment.",
                 "wordId": "7",
                 "source": "1"
         },
         {
-                "description": "The meaning associated with an artefact can change substantially as a function of the general _cultural context_ in which the artefact is emerged.",
+                "description": "The meaning associated with an artefact can change substantially as a function of the general _Cultural Context_ in which the artefact is emerged.",
                 "wordId": "8",
                 "source": "2, 3"
         },
         {
-                "description": "_use_ accounts for how people understand and interact with their artefacts in their own terms and for their own reasons.",
+                "description": "_Use_ accounts for how people understand and interact with their artefacts in their own terms and for their own reasons.",
                 "wordId": "9",
                 "source": "4"
         },
         {
-                "description": "_language_ accounts for how artefacts also occur in conversations among people, not only in user interactions.",
+                "description": "_Language_ accounts for how artefacts also occur in conversations among people, not only in user interactions.",
                 "wordId": "10",
                 "source": "4"
         },
         {
-                "description": "_life cycle_ accounts for how an artefact undergoes transformations from its conception to its retirement, and in that process must enrol stakeholders to form networks through which it can travel with ease and direction.",
+                "description": "_Life Cycle_ accounts for how an artefact undergoes transformations from its conception to its retirement, and in that process must enrol stakeholders to form networks through which it can travel with ease and direction.",
                 "wordId": "11",
                 "source": "4"
         },
         {
-                "description": "_ecologies_ account for how different species of artefacts interact with one another, compete or cooperate, and form technological complexes.",
+                "description": "_Ecologies_ account for how different species of artefacts interact with one another, compete or cooperate, and form technological complexes.",
                 "wordId": "12",
                 "source": "4"
         },
